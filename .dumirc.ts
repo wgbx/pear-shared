9f4cd180c6f349644d3a9a2b2631b1d7a8de3b48
Zhiyong Song <zhiyong.song.ext@1m.app>
2026-03-30 11:23:57 +0800

    Enhance dumi configuration to support SVG imports

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,10 +1,29 @@
+import path from 'path';
 import { defineConfig } from 'dumi';
+
+const absSrc = path.resolve(__dirname, 'src');
 
 export default defineConfig({
   alias: {
     '@svg': '/src/svg',
     '@hooks': '/src/hooks',
     '@components': '/src/components',
+  },
+  chainWebpack(memo) {
+    memo.module.rule('asset').oneOf('fallback').exclude.add(/\.svg$/i);
+    memo.module.rule('svgr').exclude.add(absSrc);
+    memo.module.rule('svg').exclude.add(absSrc);
+    memo.module
+      .rule('svg-src-as-component')
+      .enforce('pre')
+      .test(/\.svg$/i)
+      .include.add(absSrc)
+      .end()
+      .issuer(/\.[jt]sx?$/)
+      .type('javascript/auto')
+      .use('@svgr/webpack')
+      .loader(require.resolve('@svgr/webpack'))
+      .options({ typescript: true });
   },
   outputPath: 'docs-dist',
   themeConfig: {
