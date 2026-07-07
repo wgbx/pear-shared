2ae6417c55d334feb8b28788b4d32a1cf99688f5
Zhiyong Song <zhiyong.song.ext@1m.app>
2026-07-07 19:09:30 +0800

    chore: release 0.1.15

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,10 +1,13 @@
 import { styled } from '@mui/material';
+import { useMemoizedFn } from 'ahooks';
 import {
   forwardRef,
+  memo,
   useEffect,
   useState,
   type ReactElement,
   type Ref,
+  type SyntheticEvent,
 } from 'react';
 
 import { DEFAULT_IMAGE_FALLBACK } from '../../constants/image';
@@ -22,55 +25,61 @@ const StyledImage = styled('img', {
   ...(fill ? { width: '100%', height: '100%' } : undefined),
 }));
 
-export const Image = forwardRef(function Image(
-  {
-    src,
-    fallbackSrc = DEFAULT_IMAGE_FALLBACK,
-    width,
-    height,
-    fill,
-    disableOptimize,
-    quality,
-    strategy,
-    alt,
-    loading,
-    onError,
-    slotProps,
-    ...restProps
-  }: ImageProps,
-  ref: Ref<HTMLImageElement>,
-): ReactElement {
-  const optimizedSrc = useOptimizedImageUrl(src ?? '', {
-    width,
-    height,
-    quality,
-    strategy,
-    disabled: !src || disableOptimize,
-  });
+export const Image = memo(
+  forwardRef(function Image(
+    {
+      src,
+      fallbackSrc = DEFAULT_IMAGE_FALLBACK,
+      width,
+      height,
+      fill,
+      disableOptimize,
+      quality,
+      strategy,
+      alt,
+      loading,
+      onError,
+      slotProps,
+      ...restProps
+    }: ImageProps,
+    ref: Ref<HTMLImageElement>,
+  ): ReactElement {
+    const optimizedSrc = useOptimizedImageUrl(src ?? '', {
+      width,
+      height,
+      quality,
+      strategy,
+      disabled: !src || disableOptimize,
+    });
 
-  const [useFallback, setUseFallback] = useState(!src);
+    const [useFallback, setUseFallback] = useState(!src);
 
-  useEffect(() => {
-    setUseFallback(!src);
-  }, [src]);
+    useEffect(() => {
+      setUseFallback(!src);
+    }, [src]);
 
-  return (
-    <StyledImage
-      {...restProps}
-      {...slotProps?.root}
-      ref={ref}
-      src={useFallback ? fallbackSrc : optimizedSrc}
-      alt={alt}
-      width={fill ? undefined : width}
-      height={fill ? undefined : height}
-      fill={fill}
-      loading={loading ?? 'lazy'}
-      onError={(event) => {
+    const handleError = useMemoizedFn(
+      (event: SyntheticEvent<HTMLImageElement>) => {
         setUseFallback(true);
         onError?.(event);
-      }}
-    />
-  );
-});
+      },
+    );
+
+    return (
+      <StyledImage
+        {...restProps}
+        {...slotProps?.root}
+        ref={ref}
+        src={useFallback ? fallbackSrc : optimizedSrc}
+        alt={alt}
+        width={fill ? undefined : width}
+        height={fill ? undefined : height}
+        fill={fill}
+        loading={loading ?? 'lazy'}
+        onError={handleError}
+      />
+    );
+  }),
+);
 
 Image.displayName = 'Image';
